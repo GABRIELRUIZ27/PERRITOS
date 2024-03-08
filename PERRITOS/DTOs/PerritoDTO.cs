230c6add8387b087cf2701ef69c3c062febeeff8
GABRIEL <g27r02@gmail.com>
2024-03-07 23:29:57 -0600

    :zap: ADOPCION

--- PERRITOS/DTOs/PerritoDTO.cs
+++ PERRITOS/DTOs/PerritoDTO.cs
@@ -5,12 +5,12 @@ namespace Perritos.DTOs
     public class PerritoDTO
     {
         public int? Id { get; set; }
-        public string Nombre { get; set; }
-        public GeneroDTO Genero { get; set; }
-        public bool Esterilizado { get; set; }
-        public string Edad { get; set; }
+        public string? Nombre { get; set; }
+        public GeneroDTO? Genero { get; set; }
+        public bool? Esterilizado { get; set; }
+        public string? Edad { get; set; }
         public string? Imagen {  get; set; }
-        public string ImagenBase64 { get; set; }
+        public string? ImagenBase64 { get; set; }
         public DiscapacidadDTO? Discapacidad { get; set; }
     }
 }
